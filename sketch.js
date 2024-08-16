549328ab620a33ba96a5586eeca373e94716648e
amaral-w55 <joao.bulsing.amaral@escola.pr.gov.br>
2024-08-16 14:01:22 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
     createCanvas(400, 400);
+     background(0);
   }
   
   function draw() {
-    background(220);
+   
+    
+    stroke ('blue');
+    fill ("red");
+    
+    if (mouseIsPressed){
+      
+      rect (mouseX, mouseY, 20, 35);
+    }
   }
